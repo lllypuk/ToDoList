efeed6b30ddbe42f8e09942cf55ee6d6ce5ccb05
ashatrov <ashatrov@nas.company>
2020-06-20 22:39:47 +0500

    First commit

---
+++ src/test/java/ru/shatrov/todolist/ToDoListAppTests.java
@@ -0,0 +1,13 @@
+package ru.shatrov.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
