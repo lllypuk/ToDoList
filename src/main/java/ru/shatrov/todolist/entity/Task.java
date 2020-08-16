82c3bdf8a1ec95576bad17e1c839bbbfd75bd5a8
ashatrov <ashatrov@nas.company>
2020-08-16 19:52:18 +0500

    Настройки подключения к БД

--- src/main/java/ru/shatrov/todolist/entity/Task.java
+++ src/main/java/ru/shatrov/todolist/entity/Task.java
@@ -1,27 +1,34 @@
 package ru.shatrov.todolist.entity;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "task")
 public class Task {
 
     @Id
     private String id;
 
+    @Indexed
     private String todo;
+
+    private Boolean done = false;
 
     public Task() {
     }
 
-    public Task(String id, String todo) {
+    public Task(String id, String todo, Boolean done) {
         this.id = id;
         this.todo = todo;
+        this.done = done;
     }
 
     @Override
     public String toString() {
         return String.format(
-                "Task[id=%s, todo='%s']",
-                id, todo);
+                "Task[id=%s, todo='%s', done='%s']",
+                id, todo, done);
     }
 
     public String getId() {
@@ -38,5 +45,13 @@ public class Task {
 
     public void setTodo(String todo) {
         this.todo = todo;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
     }
 }
