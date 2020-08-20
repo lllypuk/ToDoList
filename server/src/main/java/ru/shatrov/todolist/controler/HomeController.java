19c3ca72863b2bc81c7ff7185d489a64c653ed78
ashatrov <NikonD3100>
2020-08-20 17:47:11 +0500

    туда-сюда

--- server/src/main/java/ru/shatrov/todolist/controler/HomeController.java
+++ server/src/main/java/ru/shatrov/todolist/controler/HomeController.java
@@ -1,12 +1,14 @@
 package ru.shatrov.todolist.controler;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = "/")
+    @CrossOrigin(origins = "http://localhost:3000")
     public String index() {
         return "index";
     }
