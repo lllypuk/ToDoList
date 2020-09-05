44abbf4ed7c57e3ad0260883a6fbb290fb273359
ashatrov <NikonD3100>
2020-09-05 19:19:46 +0500

    add lombok

--- server/src/main/java/ru/shatrov/todolist/entity/Todo.java
+++ server/src/main/java/ru/shatrov/todolist/entity/Todo.java
@@ -1,61 +1,27 @@
 package ru.shatrov.todolist.entity;
 
+import lombok.NonNull;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Data;
+
+@Data
 @Document(collection = "Todo")
 public class Todo {
 
+    @NonNull
     @Id
     private String id;
 
+    @NonNull
     private String name;
 
+    @NonNull
     private Boolean done = false;
-
-    public Todo() {
-    }
 
     public Todo(String name, Boolean done) {
         this.name = name;
-        this.done = done;
-    }
-
-    public Todo(String id, String name, Boolean done) {
-        this.id = id;
-        this.name = name;
-        this.done = done;
-    }
-
-    @Override
-    public String toString() {
-        return String.format(
-                "Task[id=%s, name='%s', done='%s']",
-                id, name, done);
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Boolean getDone() {
-        return done;
-    }
-
-    public void setDone(Boolean done) {
         this.done = done;
     }
 }
